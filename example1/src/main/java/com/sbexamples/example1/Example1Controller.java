f7236b1292c0f5b9315b1a012619191ab27b66a0
Philip Posgate <philip.posgate@gmail.com>
2017-06-03 09:25:53 -0400

    added example1 controller

---
+++ example1/src/main/java/com/sbexamples/example1/Example1Controller.java
@@ -0,0 +1,16 @@
+package com.sbexamples.example1;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Example1Controller {
+
+	@RequestMapping(value = "/hello")
+	@ResponseBody
+	public String sayHello()
+	{
+		return "Hello World!";
+	}
+}
