8f9c8742e7005576625b471f047586409a04a9c3
Philip Posgate <philip.posgate@gmail.com>
2017-06-04 18:09:24 -0400

    update example1

--- example1/src/main/java/com/sbexamples/example1/Example1Controller.java
+++ example1/src/main/java/com/sbexamples/example1/Example1Controller.java
@@ -1,13 +1,14 @@
 package com.sbexamples.example1;
 
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Example1Controller {
 
-	@RequestMapping(value = "/hello")
+	@RequestMapping(value = "/hello", method = RequestMethod.GET)
 	@ResponseBody
 	public String sayHello()
 	{
